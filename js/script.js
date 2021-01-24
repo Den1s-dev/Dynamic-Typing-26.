c41a8fea26eabe1f11aa1ed370149ba62a459eeb
Denis <legion1259@gmail.com>
2021-01-25 00:43:00 +0300

    code

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+//Превращения в строку
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//Конкатинация
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+//Превращение в число
+
+//1)
+console.log(typeof(Number('4')));
+
+//2)
+console.log(typeof(+'5'));
+
+//3)
+console.log(typeof(parseInt("15px", 10)));
+
+
+
+let answr = +prompt("Hello", "");
+
+//В булиновое значение
+// примеры всегда false
+// 0, '', null, undefined, NaN; 
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+if (switcher) {
+    console.log('Working...');
+}
+
+//2)редко использывают
+console.log(typeof(Boolean('4')));
+
+//3)
+console.log(typeof(!!"44444"));
+//Найти инфо про двойной знак отрицания!
